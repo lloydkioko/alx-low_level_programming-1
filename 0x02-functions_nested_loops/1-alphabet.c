fbf951cad5ca1dff6aea29fcb1eed5286808bb74
Winga Isaac Ronney <ronnywinga@gmail.com>
2022-09-13 19:49:33 -0700

    print alph

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- *main - print alphabet in lower case
- *print alphabet in lower case
- *_putchar can only be used twice
- *return: always (0)
- *return: nothing.
+ * print_alphabet - prints alphabet
+ *
+ * Return: always 0;
  */
-
 void print_alphabet(void)
 {
-int l = 'a';
-w
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
