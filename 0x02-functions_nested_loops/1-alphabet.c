170de7e36c6019c1fc91d46320999f7a0c04bcee
Winga Isaac Ronney <ronnywinga@gmail.com>
2022-09-13 18:40:39 -0700

    printing putch

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,19 +11,4 @@
 void print_alphabet(void)
 {
 int l = 'a';
-while (l <= 'z')
-{
-_putchar(l);
-l += 1;
-}
-_putchar(10);
-return;
-int l = 'a';
-
-while (l <= 'z')
-{
-_putchar(1);
-l += 1;
-}
-_putchar(10);
-}
+w
