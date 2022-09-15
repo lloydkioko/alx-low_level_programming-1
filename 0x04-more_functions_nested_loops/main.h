a164f588e16ae9f38b9b1e44a542ce767b8104fd
Winga Isaac Ronney <ronnywinga@gmail.com>
2022-09-15 13:25:09 -0700

    updatemain

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+int mul(int a, int b);
+int _isdigit(int c);
+int _isupper(int c);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -9,4 +13,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 void print_number(int n);
