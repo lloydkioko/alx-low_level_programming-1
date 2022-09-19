e32f187aedcd5b6618ae7e36afa9817fd5f58133
Winga Isaac Ronney <ronnywinga@gmail.com>
2022-09-19 12:27:23 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,13 @@ void print_rev(char *s)
 {
 int i = 0;
 
-while (s[i])
-i++;
-while (i--)
-_putchar(s[i]);
+while (*(s + i) != '\0')
+i += 1;
+i -= 1;
+while (i >= 0)
+{
+_putchar((s + i));
+i--;
+}
 _putchar('\n');
 }
