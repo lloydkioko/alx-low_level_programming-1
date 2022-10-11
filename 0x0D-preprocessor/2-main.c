448128ff540c4cf35b215f44a0ea3128bc78e110
Winga Isaac Ronney <ronnywinga@gmail.com>
2022-10-11 08:43:59 -0700

    2main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+
 /**
- *main  - print the name of source file.
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
  *
- * Return; 0.
+ * Return: Always 0.
  */
-int main(void)
+
+int main(void)  
 {
-printf("%s\n", _FILE_);
+printf("%s\n", __FILE__);
+
 return (0);
 }
