3dd7a2aab511b54eb87b2eb3d28dbef516d15611
Winga Isaac Ronney <ronnywinga@gmail.com>
2022-10-24 19:17:44 -0700

    created 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,29 +12,29 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	char *dup_str;
-	int len;
+list_t *new;
+char *dup_str;
+int len;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
 
-	dup_str = strdup(str);
-	if (dup_str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
+dup_str = strdup(str);
+if (dup_str == NULL)
+{
+free(new);
+return (NULL);
+}
 
-	for (len = 0; str[len];)
-		len++;
+for (len = 0; str[len];)
+len++;
 
-	new->str = dup_str;
-	new->len = len;
-	new->next = *head;
+new->str = dup_str;
+new->len = len;
+new->next = *head;
 
-	*head = new;
+*head = new;
 
-	return (new);
+return (new);
 }
